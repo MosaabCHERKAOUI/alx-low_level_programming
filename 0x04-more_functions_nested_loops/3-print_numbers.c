bf522c043afd5afea0f0c0e6b7b70e08b5b2a820
MosaabCHERKAOUI <mouschark@icloud.com>
2023-03-24 16:11:40 -0700

    h

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * print_most_numbers - print 0 - 9 apart
- *                 from 2 and 4 and you can
- *                 only use _putchar twice
  * Return: Always 0 (Success)
 */
 
@@ -13,10 +11,7 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + 48);
-		}
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_diagonal - print a diagonal line
@@ -12,14 +12,18 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
+			{
 				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+			}
+			_putchar('\\'); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
