a26dae1f412195899093ca5a22c2835c126655c2
MosaabCHERKAOUI <mouschark@icloud.com>
2023-06-21 10:27:14 -0700

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ *
+ * @name: name
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: success
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((name != NULL) && (f != NULL))
+		f(name);
+}
